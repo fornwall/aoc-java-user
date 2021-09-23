9994c536d86c87cd9c5a0e90a182375ef4596dda
Fredrik Fornwall <fredrik@fornwall.net>
2021-09-23 13:28:39 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import net.fornwall.aoc.Solver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var answer = Solver.solve(2019, 1, 1, "14");
+        System.out.println(answer);
+    }
+
+}
---
+++ src/test/java/CrashTest.java
@@ -0,0 +1,11 @@
+import net.fornwall.aoc.Solver;
+import org.junit.jupiter.api.Test;
+
+public class CrashTest {
+
+    @Test
+    void whyDoesThisCrash() {
+        var answer = Solver.solve(2019, 1, 1, "14");
+        System.out.println(answer);
+    }
+}
